d1826825991a6689f8c916d054f44dc860ac039c
Kieran Bennett <kierancliffbennett@gmail.com>
2025-10-23 12:26:38 +1100

    adding max height to modal, on mob it's going full height

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ function App() {
         className='h-screen w-screen fixed top-0'>  
         <div 
           onClick={e => e.stopPropagation()}
-          className='flex flex-col fixed m-auto bg-white justify-center w-11/12 shadow-2xl inset-0 p-4 py-6 self-center max-w-lg'
+          className='flex flex-col fixed m-auto bg-white justify-center w-11/12 shadow-2xl inset-0 p-4 py-6 self-center max-w-lg max-h-[380px]'
         >
           <h3 className='text-3xl font-sans text-center mb-6'>Enter card details</h3>
           <form className='flex flex-col gap-3 font-sans'>
